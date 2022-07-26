822b8b759cd3aa54162ae3a07a4f4b0a3fab9620
Nils Giebing <nils.giebing@gmail.com>
2022-07-26 16:13:41 +0200

    First commit

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/nilsgiebing/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0x46Fb7B21d2B499Ad4e8e1dBB5E747eCf7c58BaEd";
